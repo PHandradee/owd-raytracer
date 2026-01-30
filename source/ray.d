95ffaf4b8235fdc2a3cfec104b340f98f8e995fd
Pedro H. Andrade <63529201+PHandradee@users.noreply.github.com>
2026-01-30 10:48:56 -0300

    Implement Ray struct

---
+++ source/ray.d
@@ -0,0 +1,21 @@
+module ray;
+
+import vec3;
+
+struct Ray
+{
+    this() {}
+    this(ref const Point3 origin,ref const Vec3 direction ){
+        m_origin = origin;
+        m_direction = direction;
+    }
+
+    Point3 at(double t) const {
+        return m_origin + t * m_direction;
+    }
+
+    private {
+        Point3  m_origin;
+        Vec3    m_direction;
+    }
+}
