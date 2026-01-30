669815d890a7378811a64f80518448e540780749
Pedro H. Andrade <63529201+PHandradee@users.noreply.github.com>
2026-01-30 11:03:23 -0300

    Add origin and direction property to Ray

--- source/ray.d
+++ source/ray.d
@@ -4,18 +4,34 @@ import vec3;
 
 struct Ray
 {
-    this() {}
-    this(ref const Point3 origin,ref const Vec3 direction ){
+    this()
+    {
+    }
+
+    this(ref const Point3 origin, ref const Vec3 direction)
+    {
         m_origin = origin;
         m_direction = direction;
     }
 
-    Point3 at(double t) const {
+    @property origin() const
+    {
+        return m_origin;
+    }
+
+    @property direction() const
+    {
+        return m_direction;
+    }
+
+    Point3 at(double t) const
+    {
         return m_origin + t * m_direction;
     }
 
-    private {
-        Point3  m_origin;
-        Vec3    m_direction;
+    private
+    {
+        Point3 m_origin;
+        Vec3 m_direction;
     }
 }
